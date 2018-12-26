0f6c24bad173d18a444a3cf013684fee4b2044df
wxf980444055 <980444055@qq.com>
2018-12-26 22:55:30 +0800

    修改为有else分支之后，就可以运行成功了

--- test_web_first/views.py
+++ test_web_first/views.py
@@ -5,28 +5,31 @@ from django.shortcuts import HttpResponse
 
 # Create your views here.
 
-usr_list = [
-    {"user": "jack", "pwd": "abc"},
-    {"user": "todm", "pwd": "ABC"},
+user_list = [
+    {"user":"jack", "pwd":"abc"},
+    {"user":"todm", "pwd":"ABC"},
 ]
 
 
-def index(request):
+# def index(request):
     # request.POST
     # request.GET
     # return HttpResponse("nide qing qiu wo shou dao le!")
     # return render(request, "index.html")
 
-    if request.method == "POST":
-        usrname = request.POST.get("usrname", None)
-        password = request.POST.get("password", None)
-        print(usrname, password)
-    return render(request, "index.html",)
-
     # if request.method == "POST":
-    #     username = request.POST.get("username", None)
+    #     usrname = request.POST.get("usrname", None)
     #     password = request.POST.get("password", None)
-    #     print(username, password)
-    #     temp = {"user": username, "pwd": password}
-    #     usr_list.append(temp)
-    #     return render(request, "index.html", {"datat": usr_list})
+    #     print(usrname, password)
+    # return render(request, "index.html",)
+
+def index(request):
+    if request.method == "POST":
+        username = request.POST.get("username", None)
+        password = request.POST.get("password", None)
+        # print(username, password)
+        temp = {"user":username,"pwd":password}
+        user_list.append(temp)
+    else:
+        user_list = [{"user":"haha", "pwd":"wozhidao"}]
+        return render(request, "index.html",{"data":user_list})
